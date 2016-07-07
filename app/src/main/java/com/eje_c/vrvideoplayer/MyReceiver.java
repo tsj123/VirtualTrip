0949cad5725df4c85dfaeab0990cd1cae8c0e302
Julien <julien@ainolabs.com>
2016-07-07 15:05:17 +0200

    création du receiver (extends BroadcastReceiver)

---
+++ app/src/main/java/com/eje_c/vrvideoplayer/MyReceiver.java
@@ -0,0 +1,20 @@
+package com.eje_c.vrvideoplayer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Falls on 07/07/2016.
+ */
+public class MyReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+
+        // to do
+        
+
+    }
+
+}
